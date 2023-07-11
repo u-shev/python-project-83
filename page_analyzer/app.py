a5bd7418bfd75e3b261cb89d8a24da351c71ca92
u-shev <sheveleva.jul@gmail.com>
2023-07-11 14:07:06 +0300

    step 2 linter fix 	modified:   page_analyzer/__init__.py 	modified:   page_analyzer/app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template(
         'main.html',
-        title='Анализатор страниц',
-)
+        title='Анализатор страниц')
