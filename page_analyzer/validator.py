9849372b40a9ef0b8e68c71adbca60ff88bab196
u-shev <sheveleva.jul@gmail.com>
2023-09-01 18:01:45 +0300

    hexlet lint fix 	modified:   page_analyzer/validator.py

--- page_analyzer/validator.py
+++ page_analyzer/validator.py
@@ -7,7 +7,7 @@ def validate(url):
     normalized_url = normalize_url(url)
     if not url:
         errors['no_url'] = "URL обязателен"
-    if url and (not validators.url(normalized_url) or
-                len(normalized_url) > 255):
+    if url and (not validators.url(normalized_url)
+                or len(normalized_url) > 255):
         errors['incorrect_url'] = 'Некорректный URL'
     return errors
